b5f755675c118bb8c2432606d86c7d6b7d98f152
Mathieu <mathieu.langumier@gmail.com>
2025-02-21 14:07:02 +0100

    Even listeners

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   imports: [],
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.scss'
+  styleUrl: './counter.component.scss',
 })
 export class CounterComponent {
+  counter = signal(0);
 
+  increment() {
+    this.counter.update((value) => value + 1);
+  }
+  decrement() {
+    this.counter.update((value) => value - 1);
+  }
+  reset() {
+    this.counter.set(0);
+  }
 }
